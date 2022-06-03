da7bc85214e8d319271513e213f47ebc67d3cd77
Nathan King <nathanking3590@gmail.com>
2022-06-03 15:53:51 +0100

    add jest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import express from 'express'
-import DIContainer from './IOC/DIContainer'
+import cors from 'cors'
+import { PORT } from './Config/Config'
+import Server from './Core/Server'
+import DefaultRouter from './Routing/DefaultRouter'
+import DefaultController from './Controllers/DefaultController'
+import VideoRouter from './Routing/Video/VideoRouter'
+import VideoController from './Controllers/Video/VideoController'
 
-const container = new DIContainer(express())
+const app = express()
 
-container.Server.start()
+app.use(cors({
+    origin: `http://localhost:${PORT}`
+}))
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const routers = [
+    new DefaultRouter(app, new DefaultController),
+    new VideoRouter(app, new VideoController)
+]
+
+const server = new Server(app, routers)
+
+server.start()
